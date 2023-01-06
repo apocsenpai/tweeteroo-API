c339f6b8cf071a14b79e2a84d9e5371c46fb08f3
apocsenpai <jonatassennas@ufrrj.br>
2023-01-06 15:13:39 -0300

    feat(tweetList): show tweet list

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ server.use(express.json());
 
 const userList = [];
 const tweetList = [];
-const user = {};
 
 server.post("/sign-up", (request, response) => {
   const newUserData = request.body;
@@ -22,7 +21,17 @@ server.post("/tweets", (request, response) => {
   tweetList.push(newTweet);
   console.log("OK");
 });
-
+server.get("/tweets", (request, response) => {
+  const lastTweetIndex = tweetList.length-1;
+  let counter = 0;
+  const lastTenTweets = [];
+  while(counter<10){
+    lastTenTweets.push(tweetList[lastTweetIndex-counter]);
+    counter++;
+  }
+  console.log(lastTenTweets);
+  response.send(lastTenTweets);
+});
 server.listen(PORT, () => {
   console.log(`Você está na porta: ${PORT}`);
 });
