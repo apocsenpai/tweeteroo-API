9ee9cc1310325697785466f01fe799459a7ccbcd
apocsenpai <jonatassennas@ufrrj.br>
2023-01-06 15:09:26 -0300

    feat(signUp): allow user to publish tweets

--- src/server.js
+++ src/server.js
@@ -7,12 +7,19 @@ server.use(cors());
 server.use(express.json());
 
 const userList = [];
+const tweetList = [];
 const user = {};
 
 server.post("/sign-up", (request, response) => {
   const newUserData = request.body;
   userList.push(newUserData);
   console.log(userList)
+  console.log("OK");
+});
+
+server.post("/tweets", (request, response) => {
+  const newTweet = request.body;
+  tweetList.push(newTweet);
   console.log("OK");
 });
 
