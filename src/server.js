b3f49633076344c459051b88638581bd0649f9f5
apocsenpai <jonatassennas@ufrrj.br>
2023-01-07 15:34:08 -0300

    refactor: send username through the headers

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ const MAX_TWEETS = 10;
 
 server.post("/sign-up", (request, response) => {
   const newUserData = request.body;
-  const {username, avatar} = newUserData;
-  if(!username || !avatar){
+  const { username, avatar } = newUserData;
+  if (!username || !avatar) {
     response.status(400).send(`Todos os campos são obrigatórios!`);
     return;
   }
@@ -22,22 +22,28 @@ server.post("/sign-up", (request, response) => {
 });
 
 server.post("/tweets", (request, response) => {
-  const newTweet = request.body;
-  const {username, tweet} = newTweet;
-  if(!username || !tweet){
+  const { tweet } = request.body;
+  const { user: username } = request.headers;
+  const newTweet = { username, tweet };
+  if (!username || !tweet) {
     response.status(400).send(`Todos os campos são obrigatórios!`);
     return;
-  } else if (!userList.some((u) => u.username === newTweet.username)) {
+  } else if (!userList.some((u) => u.username === username)) {
     response.status(401).send("Unauthorized");
     return;
   }
-  const avatar = userList.find((u) => newTweet.username === u.username).avatar;
+  const avatar = userList.find((u) => username === u.username).avatar;
   tweetList.push({ ...newTweet, avatar });
   response.status(201).send("OK");
 });
 server.get("/tweets", (request, response) => {
   const lastTweets = [...tweetList].reverse();
   response.send(lastTweets.slice(0, MAX_TWEETS));
+});
+server.get("/tweets/:userName", (request, response) => {
+  const { userName } = request.params;
+  const userTweets = tweetList.filter((t) => t.username === userName);
+  response.send(userTweets);
 });
 server.listen(PORT, () => {
   console.log(`Você está na porta: ${PORT}`);
